b79e9d4c32a75d18f76b879ed00b89dff15e59c8
dv4mp1r3 <somebody@somewhere.on>
2021-01-21 22:03:41 +0300

    перенос приложения в докер

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define('MMVC_DEBUG', true);
-define('MMVC_PROJECT_NAMESPACE', 'ipinfo');
-
-require_once 'vendor/autoload.php';
-$config = require_once 'config.php';
-$app = new mmvc\core\Application($config);
-$app->run();
---
+++ public/index.php
@@ -0,0 +1,9 @@
+<?php
+
+define('MMVC_DEBUG', true);
+define('MMVC_PROJECT_NAMESPACE', 'ipinfo');
+
+require_once '../vendor/autoload.php';
+$config = require_once '../config.php';
+$app = new mmvc\core\Application($config);
+$app->run();
